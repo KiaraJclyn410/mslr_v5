a2216d074bbfe38e57b023675472486e600b0e80
KiaraJclyn410 <Kiarakim_1977@outlook.com>
2025-07-02 20:20:50 -0500

    Finished Odom V1

---
+++ src/classes.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <utility>
+#include "tracking_system.hpp"
+
+//vector class definition
+class vector {
+    public:
+    //attributes
+    double x;
+    double y;
+    double polar_angle; //in radians
+    double magnitude;
+
+    //methods
+    //setter
+    void set_coords(double x_coord, double y_coord){
+        x = x_coord;
+        y = y_coord;
+    }
+    void set_polar(double angle_in_radians, double magnitude) {
+        polar_angle = angle_in_radians;
+        magnitude = magnitude;
+    }
+    //cartesian to polar conversion
+    std::pair<double, double> cartesian_to_polar() const {
+        double magnitude = sqrt(x*x + y*y);
+        double polar_angle = atan2(y,x);
+        return std::make_pair(magnitude, polar_angle);
+    }
+
+    //polar to cartesian conversion
+    std::pair<double, double> polar_to_cartesian(double magnitude, double polar_angle) const {
+        double x_coord = magnitude * cos(polar_angle);
+        double y_coord = magnitude * sin(polar_angle);
+        return std::make_pair(x_coord, y_coord);
+    }
+
+    //field to robot global conversion
+    std::pair<double, double> field_to_robot_global() const {
+        double field_global_x = x + robot_to_field_x_scaling + start_vertical;
+        double field_global_y = y + robot_to_field_y_scaling + start_horizontal;
+        return std::make_pair(field_global_x, field_global_y);
+    }
+};
--- src/tracking_system.cpp
+++ src/tracking_system.cpp
@@ -4,30 +4,32 @@
 #include "pros/rotation.hpp"
 #include <numbers>
 #include <list>
+#include "classes.cpp"
 
 double pi = std::numbers::pi;
 
 
 void tracking_loop (){
+    //get the starting value for this cycle
+    start_horizontal = horizontal_rotation.get_position();
+    start_vertical = vertical_rotation.get_position();
+    double start_angle = imu_sensor.get_rotation();
     while (true){
-        //get the starting value for this cycle
-        double start_horizontal = horizontal_rotation.get_position();
-        double start_vertical = vertical_rotation.get_position();
-        double start_angle = imu_sensor.get_rotation();
 
+        //replace later with stuff
         //get the current values for this cycle
         double horizontal_wheel = horizontal_rotation.get_position();
         double vertical_wheel = vertical_rotation.get_position();
         double angle = imu_sensor.get_rotation();
 
         //get the differences to use in this timeframe's calculations
-        double distance_vertical = vertical_wheel - start_vertical;
-        double distance_horizontal = horizontal_wheel - start_horizontal;
-        double difference_angle = angle - start_angle;
+        double delta_vertical = vertical_wheel - start_vertical;
+        double delta_horizontal = horizontal_wheel - start_horizontal;
+        double delta_angle = angle - start_angle;
 
         //update the starting values for the next cycle
-        start_horizontal = start_horizontal + distance_horizontal;
-        start_vertical = start_vertical + distance_vertical;
+        start_horizontal = start_horizontal + delta_horizontal;
+        start_vertical = start_vertical + delta_vertical;
 
         //normalize "angle" here
         double normalized_angle = fmod(angle, 360.0);
@@ -36,17 +38,49 @@ void tracking_loop (){
         double angle_in_radians = normalized_angle * (pi / 180.0);
 
         //find the arc radius
-        double arc_radius = distance_vertical/angle_in_radians + vertical_wheel_to_center_distance;
+        double vertical_arc_radius = delta_vertical/angle_in_radians + vertical_wheel_to_center_distance;//+ or -
+        double horizontal_arc_radius = delta_horizontal/angle_in_radians + horizontal_wheel_to_center_distance;//+ or -
 
         //find the chord length
-        double y_axis = 2 * arc_radius * sin(angle_in_radians/2);
+        double chord_length = delta_vertical/delta_angle + vertical_wheel_to_center_distance;
 
-        //calculate the shift, aka x-axis
-        double x_axis = horizontal_wheel/angle_in_radians + horizontal_wheel_to_center_distance;
+        //find vector in robot_local
+        int robot_local_x = chord_length;
+        int robot_local_y = delta_horizontal/delta_angle + horizontal_wheel_to_center_distance;
 
-        //calculate the distance vector in the local coordinate system
-        double local_vector_y = 2 * sin(angle_in_radians/2) * y_axis;
-        double local_vector_x = 2 * sin(angle_in_radians/2) * x_axis;
-        std::local_vector<double> = {local_vector_x, local_vector_y};
+        //create a vector in robot local with our vector class
+        //this vector is in cartesian
+        vector robot_local_vector;
+        robot_local_vector.set_coords(robot_local_x, robot_local_y);
+
+        //convert to polar coordinates
+        std::pair<double, double> robot_local_vector_polar = robot_local_vector.cartesian_to_polar();
+        //vars
+        double magnitude = robot_local_vector_polar.first;
+        double polar_angle = robot_local_vector_polar.second;
+
+        //rotate the anlge to the robot_global
+        double offset_angle = (start_angle + angle)/2;
+        //convert to radians
+        double offset_angle_in_radians = offset_angle * (pi / 180.0);
+        //add the offset angle to the polar angle
+        double robot_global_angle = polar_angle + offset_angle;//+or-
+        //let's convert it to cartesian
+        vector robot_global_vector_polar;
+        robot_global_vector_polar.set_polar(robot_global_angle, magnitude);
+        //now we have the robot global vector in polar
+
+        //convert to cartesian
+        std::pair<double, double> robot_global_vector_cartesian_coords = robot_global_vector_polar.polar_to_cartesian(magnitude, robot_global_angle);
+        double robot_global_x = robot_global_vector_cartesian_coords.first;
+        double robot_global_y = robot_global_vector_cartesian_coords.second;
+
+        //now we have the robot global vector in cartesian
+        vector robot_global_vector_cartesian;
+        robot_global_vector_cartesian.set_coords(robot_global_x, robot_global_y);
+
+        //shift to the field_global plane
+        std::pair<double, double> field_global_vector_= robot_global_vector_cartesian.field_to_robot_global();
+        double field_global_x = field_global_vector_.first;
+        double field_global_y = field_global_vector_.second;
     }
-}
